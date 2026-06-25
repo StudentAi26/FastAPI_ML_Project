d3c32103a51a1c7c905662fa08238234e2eeeb57
StudentAi26 <aida.sky@bk.ru>
2026-06-26 00:33:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "API работает"}
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Dogs vs Cats Classification API"}
